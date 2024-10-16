8031703d61a93d3ff2c823b5d0cbb7131b40e1ee
Mike Ferrari <mferrari@swiftcom.com>
2024-10-16 16:07:27 -0700

    updated from app to individual components

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AuthComponent from './AuthComponent';
+import { Amplify } from 'aws-amplify';
+import AuthenticatorComponent from './AuthenticatorComponent';
+import NewsletterComponent from './NewsletterComponent';
+import HeadlineComponent from './HeadlineComponent';
+import OnboardingComponent from './OnboardingComponent';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('auth-root'));
-root.render(
-  <React.StrictMode>
-    <AuthComponent />
-  </React.StrictMode>
-);
+// Configure Amplify
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: false,
+    region: 'us-west-2',
+    userPoolId: 'us-west-2_i3wjFy9vc',
+    userPoolWebClientId: 's12edm6otcvdr0le10aojt4bc'
+  }
+});
+
+const renderComponent = (Component, elementId) => {
+  const element = document.getElementById(elementId);
+  if (element) {
+    ReactDOM.createRoot(element).render(
+      <React.StrictMode>
+        <Component />
+      </React.StrictMode>
+    );
+  }
+};
+
+renderComponent(AuthenticatorComponent, 'loginhere');
+renderComponent(NewsletterComponent, 'newsletters');
+renderComponent(HeadlineComponent, 'bookmarks');
+renderComponent(OnboardingComponent, 'onboardme');
